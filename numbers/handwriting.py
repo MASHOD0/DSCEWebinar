0dc8e9962811c666c268c91c27d1108659fb179c
MASHOD0 <mashhood2002@gmail.com>
2021-01-09 00:21:51 +0530

    added all the code

--- numbers/handwriting.py
+++ numbers/handwriting.py
@@ -44,7 +44,7 @@ model.compile(
     loss="categorical_crossentropy",
     metrics=["accuracy"]
 )
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train, y_train, epochs=20)
 
 # Evaluate neural network performance
 model.evaluate(x_test,  y_test, verbose=2)
